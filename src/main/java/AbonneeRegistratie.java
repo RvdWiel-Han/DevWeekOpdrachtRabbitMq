1f2bcd66ab7f52f61f1afdceadbfbad99d8b5b8c
Roy van de Wiel <R.vandeWiel@student.han.nl>
2023-11-02 16:09:22 +0100

    Added main to application

--- src/main/java/AbonneeRegistratie.java
+++ src/main/java/AbonneeRegistratie.java
@@ -1,19 +1,54 @@
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 public class AbonneeRegistratie {
 
     CreditcardValidateClient creditcardValidateClient = new CreditcardValidateClient();
-    NotificatieClient notificatieClient =  new NotificatieClient();
+    NotificatieClient notificatieClient = new NotificatieClient();
 
-    public AbonneeRegistratie() throws IOException, TimeoutException {
-
+    public AbonneeRegistratie() throws IOException, TimeoutException, ExecutionException, InterruptedException {
+        // Start listening on port 8000
+        startListening(8000);
     }
 
     public void nieuweAbonnee(AbonneeAanvraag aanvraag) throws IOException, ExecutionException, InterruptedException, TimeoutException {
-        if(creditcardValidateClient.valideerCreditcard(aanvraag.creditcard)){
+        if (creditcardValidateClient.valideerCreditcard(aanvraag.creditcard)) {
             notificatieClient.notificeerExternen();
         }
+    }
+
+    private void startListening(int port) throws ExecutionException, InterruptedException, TimeoutException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Listening on port " + port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // Wait for a client to connect
+                System.out.println("Client connected from " + clientSocket.getInetAddress());
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    String creditCardData = reader.readLine(); // Read the credit card data from the client
+
+                    // Create an AbonneeAanvraag with the received credit card data
+                    AbonneeAanvraag aanvraag = new AbonneeAanvraag(creditCardData);
+
+                    // Call the nieuweAbonnee method to process the request
+                    nieuweAbonnee(aanvraag);
+
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, TimeoutException, ExecutionException, InterruptedException {
+        new AbonneeRegistratie();
     }
 }
